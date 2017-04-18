715aa08d2f8031a417e4bf36571d2fb9979062f8
maratgaip <marattig@gmail.com>
2017-04-17 22:37:01 -0700

    test

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/api/radio'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
@@ -12,9 +13,7 @@ app.set('view engine', 'ejs');
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
-app.get('/api/radio', function(request, response) {
-  response.render('pages/index');
-});
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
